f53d58cb0e8edbe7b7e929e0a2dfa232d5e38043
koga1020 <koga.shozo1020@gmail.com>
2022-04-26 21:28:46 +0900

    generate Comment schema

--- test/support/fixtures/blog_fixtures.ex
+++ test/support/fixtures/blog_fixtures.ex
@@ -18,4 +18,18 @@ defmodule DataloaderSample.BlogFixtures do
 
     post
   end
+
+  @doc """
+  Generate a comment.
+  """
+  def comment_fixture(attrs \\ %{}) do
+    {:ok, comment} =
+      attrs
+      |> Enum.into(%{
+        body: "some body"
+      })
+      |> DataloaderSample.Blog.create_comment()
+
+    comment
+  end
 end
