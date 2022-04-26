b2b6548411cbf60e10dc14c15e5ee07ed2a94718
koga1020 <koga.shozo1020@gmail.com>
2022-04-26 21:45:51 +0900

    add absinthe

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule DataloaderSample.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:faker, "~> 0.17", only: [:dev, :test]}
+      {:faker, "~> 0.17", only: [:dev, :test]},
+      {:absinthe, "~> 1.6.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
