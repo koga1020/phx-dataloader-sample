c4bbb326b87d1750b490636fd8f9e7141f842e69
koga1020 <koga.shozo1020@gmail.com>
2022-04-26 21:37:25 +0900

    add faker

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule DataloaderSample.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:faker, "~> 0.17", only: [:dev, :test]}
     ]
   end
 
