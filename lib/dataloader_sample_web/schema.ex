c581ea90260426d67bc0278b22557b4f205b2e6e
koga1020 <koga.shozo1020@gmail.com>
2022-04-26 22:23:02 +0900

    add comments field(N+1 version)

--- lib/dataloader_sample_web/schema.ex
+++ lib/dataloader_sample_web/schema.ex
@@ -7,6 +7,18 @@ defmodule DataloaderSampleWeb.Schema do
     field :id, non_null(:id)
     field :title, non_null(:string)
     field :body, non_null(:string)
+
+    field :comments, non_null(list_of(:comment)) do
+      resolve(fn post, _, _ ->
+        comments = Ecto.assoc(post, :comments) |> DataloaderSample.Repo.all()
+        {:ok, comments}
+      end)
+    end
+  end
+
+  object :comment do
+    field :id, non_null(:id)
+    field :body, non_null(:string)
   end
 
   query do
